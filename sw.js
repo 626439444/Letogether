68bb882fbbaf6a0d558eb00327c1a19b453c9975
Xin <13612584220@139.com>
2026-02-28 23:14:25 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('PWA Service Worker 已安装');
-});
-
-self.addEventListener('fetch', (e) => {
-  // 这里可以留空，或者处理缓存逻辑
-  e.respondWith(fetch(e.request));
-});
